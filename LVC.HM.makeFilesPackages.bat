5945fa09518b7da8a6b9796ec0273cfbaf3c1e76
Tobias B. Besemer <Tobias.Besemer@Googlemail.com>
2017-08-04 12:49:22 +0200

    Core + Module

--- LVC.HM.makeFilesPackages.bat
+++ LVC.HM.makeFilesPackages.bat
@@ -1,4 +1,4 @@
-@echo off
+// @echo off
 
 rem LVC.HM.makeFilesPackages.bat
 
@@ -12,12 +12,12 @@ copy .\Branding\LV-Crew.HostsManager.Logo.png .\LVC.HM.Packages\LVC.HM.Win-x64.S
 copy .\Branding\LV-Crew.HostsManager.Logo.png .\LVC.HM.Packages\LVC.HM.Win-x86.Archive\LV-Crew.HostsManager.Logo1.png
 copy .\Branding\LV-Crew.HostsManager.Logo.png .\LVC.HM.Packages\LVC.HM.Win-x86.Setup\LV-Crew.HostsManager.Logo1.png
 
-copy .\LVC.HM.Readme.txt .\LVC.HM.Packages\LVC.HM.Win-x64.Archive\Readme.txt
-copy .\LVC.HM.Readme.txt .\LVC.HM.Packages\LVC.HM.Win-x64.Setup\Readme.txt
-copy .\LVC.HM.Readme.txt .\LVC.HM.Packages\LVC.HM.Win-x86.Archive\Readme.txt
-copy .\LVC.HM.Readme.txt .\LVC.HM.Packages\LVC.HM.Win-x86.Setup\Readme.txt
+copy .\Readme.txt .\LVC.HM.Packages\LVC.HM.Win-x64.Archive\Readme.txt
+copy .\Readme.txt .\LVC.HM.Packages\LVC.HM.Win-x64.Setup\Readme.txt
+copy .\Readme.txt .\LVC.HM.Packages\LVC.HM.Win-x86.Archive\Readme.txt
+copy .\Readme.txt .\LVC.HM.Packages\LVC.HM.Win-x86.Setup\Readme.txt
 
-copy .\LVC.HM.License.rtf .\LVC.HM.Packages\LVC.HM.Win-x64.Archive\License.rtf
-copy .\LVC.HM.License.rtf .\LVC.HM.Packages\LVC.HM.Win-x64.Setup\License.rtf
-copy .\LVC.HM.License.rtf .\LVC.HM.Packages\LVC.HM.Win-x86.Archive\License.rtf
-copy .\LVC.HM.License.rtf .\LVC.HM.Packages\LVC.HM.Win-x86.Setup\License.rtf
+copy .\License.rtf .\LVC.HM.Packages\LVC.HM.Win-x64.Archive\License.rtf
+copy .\License.rtf .\LVC.HM.Packages\LVC.HM.Win-x64.Setup\License.rtf
+copy .\License.rtf .\LVC.HM.Packages\LVC.HM.Win-x86.Archive\License.rtf
+copy .\License.rtf .\LVC.HM.Packages\LVC.HM.Win-x86.Setup\License.rtf
--- LVC.HM.makePackages.bat
+++ LVC.HM.makePackages.bat
@@ -1,4 +1,4 @@
-@echo off
+// @echo off
 
 rem LVC.HM.Packages.bat
 
