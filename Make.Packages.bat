8555f62b3cde057ad4c38daf1cf01a006945890e
Tobias B. Besemer <Tobias.Besemer@Googlemail.com>
2017-08-27 15:33:25 +0200

    2017-08-27a

--- Make.Packages.bat
+++ Make.Packages.bat
@@ -1,62 +1,71 @@
-rem @echo off
-
-
-rem Make.Packages.bat
-rem Version 2017.08.05c
-
-rem For later
-rem call .\Branding\Branding.bat
-
-
-rem Copy files together
-copy .\Branding\Branding.ini .\Packages\Win-x64.Archive\
-copy .\Branding\Branding.ini .\Packages\Win-x64.Setup\
-copy .\Branding\Branding.ini .\Packages\Win-x86.Archive\
-copy .\Branding\Branding.ini .\Packages\Win-x86.Setup\
-
-copy .\Branding\LV-Crew.HostsManager.Logo.ico .\Packages\Win-x64.Archive\
-copy .\Branding\LV-Crew.HostsManager.Logo.ico .\Packages\Win-x64.Setup\
-copy .\Branding\LV-Crew.HostsManager.Logo.ico .\Packages\Win-x86.Archive\
-copy .\Branding\LV-Crew.HostsManager.Logo.ico .\Packages\Win-x86.Setup\
-
-copy .\Branding\LV-Crew.HostsManager.Logo.png .\Packages\Win-x64.Archive\
-copy .\Branding\LV-Crew.HostsManager.Logo.png .\Packages\Win-x64.Setup\
-copy .\Branding\LV-Crew.HostsManager.Logo.png .\Packages\Win-x86.Archive\
-copy .\Branding\LV-Crew.HostsManager.Logo.png .\Packages\Win-x86.Setup\
-
-copy .\Settings.xml .\Packages\Win-x64.Archive\
-copy .\Settings.xml .\Packages\Win-x64.Setup\
-copy .\Settings.xml .\Packages\Win-x86.Archive\
-copy .\Settings.xml .\Packages\Win-x86.Setup\
-
+@echo off
+cls 
+echo Make.Packages.bat
+echo Version 2017.08.27a
+echo.
+echo Copy files together...
+echo.
+echo copy .\Branding\*.* .\Packages\Win-x64.Archive\
+copy .\Branding\*.* .\Packages\Win-x64.Archive\
+echo.
+echo copy .\Branding\*.* .\Packages\Win-x64.Setup\
+copy .\Branding\*.* .\Packages\Win-x64.Setup\
+echo.
+echo copy .\Branding\*.* .\Packages\Win-x86.Archive\
+copy .\Branding\*.* .\Packages\Win-x86.Archive\
+echo.
+echo copy .\Branding\*.* .\Packages\Win-x86.Setup\
+copy .\Branding\*.* .\Packages\Win-x86.Setup\
+rem echo.
+rem echo copy .\Settings.xml .\Packages\Win-x64.Archive\
+rem copy .\Settings.xml .\Packages\Win-x64.Archive\
+rem echo copy .\Settings.xml .\Packages\Win-x64.Setup\
+rem copy .\Settings.xml .\Packages\Win-x64.Setup\
+rem echo copy .\Settings.xml .\Packages\Win-x86.Archive\
+rem copy .\Settings.xml .\Packages\Win-x86.Archive\
+rem echo copy .\Settings.xml .\Packages\Win-x86.Setup\
+rem copy .\Settings.xml .\Packages\Win-x86.Setup\
+echo.
+echo copy .\Readme.txt .\Packages\Win-x64.Archive\
 copy .\Readme.txt .\Packages\Win-x64.Archive\
+echo copy .\Readme.txt .\Packages\Win-x64.Setup\
 copy .\Readme.txt .\Packages\Win-x64.Setup\
+echo copy .\Readme.txt .\Packages\Win-x86.Archive\
 copy .\Readme.txt .\Packages\Win-x86.Archive\
+echo copy .\Readme.txt .\Packages\Win-x86.Setup\
 copy .\Readme.txt .\Packages\Win-x86.Setup\
-
+echo.
+echo copy .\License.rtf .\Packages\Win-x64.Archive\
 copy .\License.rtf .\Packages\Win-x64.Archive\
+echo copy .\License.rtf .\Packages\Win-x64.Setup\
 copy .\License.rtf .\Packages\Win-x64.Setup\
+echo copy .\License.rtf .\Packages\Win-x86.Archive\
 copy .\License.rtf .\Packages\Win-x86.Archive\
+echo copy .\License.rtf .\Packages\Win-x86.Setup\
 copy .\License.rtf .\Packages\Win-x86.Setup\
-
-
-rem Make Packages
-cd Packages
-
-rem Make Win-x64.zip
-del Win-x64.zip
-7za a Win-x64.zip .\Win-x64.Archive\*.*
-7za a Win-x64.zip .\Win-x64.Archive\certutil
-
-rem Make Win-x64.exe
-rem "C:\Program Files (x86)\NSIS\makensis.exe" .\Win-x64.nsi
-
-rem Make Win-x86.zip
-del Win-x86.zip
-7za a Win-x86.zip .\Win-x86.Archive\*.*
-7za a Win-x86.zip .\Win-x86.Archive\certutil
-
-rem Make Win-x86.exe
-"C:\Program Files (x86)\NSIS\makensis.exe" .\Win-x86.nsi
-
-cd ..
+echo.
+echo Make Packages...
+echo.
+echo Make Win-x64.zip
+del .\Packages\Win-x64.zip
+.\Make\7za a .\Packages\Win-x64.zip .\Packages\Win-x64.Archive\*.*
+.\Make\7za a .\Packages\Win-x64.zip .\Packages\Win-x64.Archive\certutil
+echo.
+echo Make Win-x64.exe
+del .\Packages\Win-x64.exe
+rem "C:\Program Files (x86)\NSIS\makensis.exe" .\Make\Win-x64.nsi
+echo.
+echo Move EXE to .\Packages\...
+move .\Make\Win-x64.exe .\Packages\
+echo.
+echo Make Win-x86.zip
+del .\Packages\Win-x86.zip
+.\Make\7za a .\Packages\Win-x86.zip .\Packages\Win-x86.Archive\*.*
+.\Make\7za a .\Packages\Win-x86.zip .\Packages\Win-x86.Archive\certutil
+echo.
+echo Make Win-x86.exe
+del .\Packages\Win-x86.exe
+"C:\Program Files (x86)\NSIS\makensis.exe" .\Make\Win-x86.nsi
+echo.
+echo Move EXE to .\Packages\...
+move .\Make\Win-x86.exe .\Packages\
